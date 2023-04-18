481a59545e2ebd8f6a216d663bfd1e65533294d8
cjk <111162023+magicsoft7@users.noreply.github.com>
2023-04-18 22:56:33 +0900

    update java version to 11

---
+++ src/main/java/sample/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//구구단 코드 작성
+//1. 2단부터 9단까지 출력
+//2. 2단부터 9단까지 출력
+//3. 2단부터 9단까지 출력
+//4. 2단부터 9단까지 출력
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
